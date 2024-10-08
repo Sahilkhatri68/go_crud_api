6a1ec722cb9567ab1c052cb78cd88707c73497c3
Sahilkhatri68 <86000524+Sahilkhatri68@users.noreply.github.com>
2024-10-08 13:44:02 -0400

    GET By Id Function is created

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -36,10 +37,37 @@ func createCar(w http.ResponseWriter, r *http.Request) {
 }
 
 // code to get all cars
-// Get all cars (GET /cars)
 func getCars(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(cars)
+}
+
+// code to get car by ID
+
+// function to get car by ID
+func getCarByID(id int) (*Car, int) {
+	for i, car := range cars {
+		if car.ID == id {
+			return &car, i
+		}
+	}
+	return nil, -1
+}
+
+func getCar(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid car ID", http.StatusBadRequest)
+		return
+	}
+	car, _ := getCarByID(id)
+	if car == nil {
+		http.Error(w, "Car not found", http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(car)
 }
 
 func main() {
@@ -48,6 +76,7 @@ func main() {
 	// Routes for CRUD operations
 	router.HandleFunc("/cars", createCar).Methods("POST")
 	router.HandleFunc("/cars", getCars).Methods("GET")
+	router.HandleFunc("/cars/{id}", getCar).Methods("GET")
 
 	// Start the server
 	fmt.Println("Server is running on port 8080...")
